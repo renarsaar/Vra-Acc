388a7a68bc6ca674631459d675a3ce3c21a4c1a8
renarsaar <renar.saaremets@gmail.com>
2020-06-26 20:31:01 +0300

    remove unused components

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://source.unsplash.com",
-  headers: {
-    Authorization: "Client-ID JNKMTEx6XiCKF0YuhXpLTCstW3GTYNvewTKhR9h9U20",
-  },
-});
